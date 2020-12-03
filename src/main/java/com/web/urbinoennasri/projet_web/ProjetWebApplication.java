c183460580d16449eae097e09074b71ea6b4834d
so-asb93 <ennasri@et.esiea.fr>
2020-12-03 14:32:42 +0100

    Démarrage du projet

---
+++ src/main/java/com/web/urbinoennasri/projet_web/ProjetWebApplication.java
@@ -0,0 +1,13 @@
+package com.web.urbinoennasri.projet_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/urbinoennasri/projet_web/ProjetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.urbinoennasri.projet_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
