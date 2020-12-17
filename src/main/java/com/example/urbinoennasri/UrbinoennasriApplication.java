fbe8694142851db19cbd817c060c422e181b02a8
julian <urbino@et.esiea.fr>
2020-12-17 21:23:49 +0100

    fix issues related to login page

--- src/main/java/com/example/urbinoennasri/UrbinoennasriApplication.java
+++ src/main/java/com/example/urbinoennasri/UrbinoennasriApplication.java
@@ -1,9 +1,14 @@
 package com.example.urbinoennasri;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class UrbinoennasriApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/urbinoennasri/controller/Controller.java
+++ src/main/java/com/example/urbinoennasri/controller/Controller.java
@@ -3,16 +3,18 @@ package com.example.urbinoennasri.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @RequestMapping(value = { "/connexion" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/authentification" }, method = RequestMethod.GET)
+    @ResponseBody
     public ModelAndView connexion() {
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("connexion");
+        modelAndView.setViewName("authentification");
         return modelAndView;
     } // Requête pour afficher une page hmtl
 
